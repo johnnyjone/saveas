e9d5709ef3236daf37acbcfdabe47ce2a1e810c4
johnnyjone <122457558+johnnyjone@users.noreply.github.com>
2023-01-11 15:03:10 +0100

    Update 1st app.py

--- 1st app.py
+++ 1st app.py
@@ -1,23 +1,4 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
 import streamlit as st
 import pandas as pd
 import numpy as np
 import pydeck as pdk
-
-
-# In[2]:
-
-
-st.title('Editing regulatory report')
-
-
-# In[ ]:
-
-
-
-
