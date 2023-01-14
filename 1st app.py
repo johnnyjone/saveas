9a26b0d6fd5d6acd57d796910c26c2b6bdd5263d
johnnyjone <122457558+johnnyjone@users.noreply.github.com>
2023-01-14 19:10:36 +0100

    Update 1st app.py

--- 1st app.py
+++ 1st app.py
@@ -2,32 +2,18 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import pydeck as pdk
+   
 
-stocks_40 = [
-    ["Atos", "ATO.PA"],
-    ["Axa", "CS.PA"],  
-    ["Capgemini", "CAP.PA"],
-    ["Carrefour", "CA.PA"],
-    ["EssilorLuxottica", "EL.PA"],
-    ["Hermès International", "RMS.PA"],
-    ["Kering", "KER.PA"],
-    ["L'Oréal", "OR.PA"],
-    ["LVMH", "MC.PA"],
-    ["Orange", "ORA.PA"],
-    ["Publicis Groupe", "PUB.PA"],  
-    ["Sanofi", "SAN.PA"],
-    ["Stellantis", "STLA.PA"],
-    ["Teleperformance", "TEP.PA"],   
-    ["Vivendi", "VIV.PA"],
-    ["Worldline", "WLN.PA"]]
-
-A = [["Schneider Electric", "SU.PA"], ["Legrand", "LR.PA"]]
-B = [["Engie", "ENGI.PA"], ["Unibail-Rodamco-Westfield", "URW.AS"]]
-C = [["Veolia", "VIE.PA"], ["Alstom", "ALO.PA"]]
+A = [["Schneider Electric", "SU.PA"], ["Orange", "ORA.PA"], ["Legrand", "LR.PA"]]
+B = [["Engie", "ENGI.PA"], ["EssilorLuxottica", "EL.PA"], ["Unibail-Rodamco-Westfield", "URW.AS"]]
+C = [["Veolia", "VIE.PA"], ["Alstom", "ALO.PA"], ["L'Oréal", "OR.PA"], ["Worldline", "WLN.PA"], 
+     ["Hermès International", "RMS.PA"], ["Carrefour", "CA.PA"], ["Atos", "ATO.PA"]]
 D = [["Michelin", "ML.PA"], ["Bouygues", "EN.PA"], ["Danone", "BN.PA"], ["STMicroelectronics", "STM.PA"], 
-     ["Renault", "RNO.PA"], ["Peugeot", "PEUG.PA"], ["Saint-Gobain", "SGO.PA"], ["Pernod Ricard", "RI.PA"], ["Vinci", "DG.PA"]]
-E = [["Thales", "HO.PA"], ["Airbus", "AIR.PA"], ["Safran", "SAF.PA"]]
-F = [["ArcelorMittal", "MT.AS"], ["Air liquide", "AI.PA"]]
+     ["Renault", "RNO.PA"], ["Peugeot", "PEUG.PA"], ["Saint-Gobain", "SGO.PA"], ["Pernod Ricard", "RI.PA"], ["Teleperformance", "TEP.PA"],   
+     ["Vivendi", "VIV.PA"], ["Publicis Groupe", "PUB.PA"], ["Capgemini", "CAP.PA"], ["Vinci", "DG.PA"]]
+E = [["Thales", "HO.PA"], ["Sanofi", "SAN.PA"], ["Axa", "CS.PA"], ["Stellantis", "STLA.PA"], 
+     ["Airbus", "AIR.PA"], ["LVMH", "MC.PA"], ["Safran", "SAF.PA"]]
+F = [["ArcelorMittal", "MT.AS"], ["Kering", "KER.PA"], ["Air liquide", "AI.PA"]]
 G = [["BNP Paribas", "BNP.PA"], ["Dassault Systèmes", "DSY.PA"], ["Crédit agricole", "ACA.PA"], ["TotalEnergies", "TTE.PA"], ["Société générale", "GLE.PA"]]
 
 
