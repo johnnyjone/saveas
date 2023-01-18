233cce371658282167f61c0128e601f1bea2ca7a
johnnyjone <122457558+johnnyjone@users.noreply.github.com>
2023-01-18 14:51:10 +0100

    Update 1st app.py

--- 1st app.py
+++ 1st app.py
@@ -15,48 +15,16 @@ E = [["Thales", "HO.PA"], ["Sanofi", "SAN.PA"], ["Axa", "CS.PA"], ["Stellantis",
      ["Airbus", "AIR.PA"], ["LVMH", "MC.PA"], ["Safran", "SAF.PA"]]
 F = [["ArcelorMittal", "MT.AS"], ["Kering", "KER.PA"], ["Air liquide", "AI.PA"]]
 G = [["BNP Paribas", "BNP.PA"], ["Dassault Systèmes", "DSY.PA"], ["Crédit agricole", "ACA.PA"], ["TotalEnergies", "TTE.PA"], ["Société générale", "GLE.PA"]]
+indecolo = [A,B,C,D,E,F,G]
 
-
-stocks_40 = [
-    ["Air liquide", "AI.PA"],  91
-    ["Airbus", "AIR.PA"],  87
-    ["Alstom", "ALO.PA"],  94
-    ["ArcelorMittal", "MT.AS"],  82
-    ["Atos", "ATO.PA"],  86
-    ["Axa", "CS.PA"],  93
-    ["BNP Paribas", "BNP.PA"],  87
-    ["Bouygues", "EN.PA"],  83
-    ["Capgemini", "CAP.PA"],  94
-    ["Carrefour", "CA.PA"],  85
-    ["Crédit agricole", "ACA.PA"],  88
-    ["Danone", "BN.PA"],  89
-    ["Dassault Systèmes", "DSY.PA"],  94
-    ["Engie", "ENGI.PA"],  88
-    ["EssilorLuxottica", "EL.PA"],  94
-    ["Hermès International", "RMS.PA"],  92
-    ["Kering", "KER.PA"],  95
-    ["Legrand", "LR.PA"],  91
-    ["L'Oréal", "OR.PA"],  95
-    ["LVMH", "MC.PA"],  91
-    ["Michelin", "ML.PA"], 93
-    ["Orange", "ORA.PA"],  99
-    ["Pernod Ricard", "RI.PA"], 81
-    ["Peugeot", "PEUG.PA"],  89
-    ["Publicis Groupe", "PUB.PA"],  84
-    ["Renault", "RNO.PA"],  88
-    ["Safran", "SAF.PA"],  90
-    ["Saint-Gobain", "SGO.PA"],
-    ["Sanofi", "SAN.PA"], 87
-    ["Schneider Electric", "SU.PA"],  94
-    ["Société générale", "GLE.PA"],  86
-    ["Stellantis", "STLA.PA"],
-    ["STMicroelectronics", "STM.PA"],  88
-    ["Teleperformance", "TEP.PA"], 84
-    ["Thales", "HO.PA"],  89
-    ["TotalEnergies", "TTE.PA"],  90
-    ["Unibail-Rodamco-Westfield", "URW.AS"],  92
-    ["Veolia", "VIE.PA"],  88
-    ["Vinci", "DG.PA"],  83
-    ["Vivendi", "VIV.PA"],  84
-    ["Worldline", "WLN.PA"]]  88
-
+a = [["Orange", "ORA.PA"], ["Kering", "KER.PA"], ["L'Oréal", "OR.PA"]]
+b = [["Schneider Electric", "SU.PA"], ["Michelin", "ML.PA"], ["EssilorLuxottica", "EL.PA"], ["Dassault Systèmes", "DSY.PA"], 
+     ["Capgemini", "CAP.PA"], ["Axa", "CS.PA"], ["Alstom", "ALO.PA"]]  
+c = [["Safran", "SAF.PA"], ["LVMH", "MC.PA"], ["Legrand", "LR.PA"], ["Air liquide", "AI.PA"], 
+     ["Unibail-Rodamco-Westfield", "URW.AS"], ["Hermès International", "RMS.PA"]]
+d = [["Thales", "HO.PA"], ["Stellantis", "STLA.PA"], ["Peugeot", "PEUG.PA"], ["Danone", "BN.PA"], ["TotalEnergies", "TTE.PA"]]
+e = [["Crédit agricole", "ACA.PA"], ["Veolia", "VIE.PA"], ["STMicroelectronics", "STM.PA"], ["Renault", "RNO.PA"], ["Engie", "ENGI.PA"], ["Worldline", "WLN.PA"]] 
+f = [["Atos", "ATO.PA"], ["Airbus", "AIR.PA"], ["Sanofi", "SAN.PA"], ["BNP Paribas", "BNP.PA"]] 
+g = [["Vivendi", "VIV.PA"], ["Teleperformance", "TEP.PA"], ["Publicis Groupe", "PUB.PA"], ["Carrefour", "CA.PA"], ["Société générale", "GLE.PA"]]
+h = [["Pernod Ricard", "RI.PA"], ["ArcelorMittal", "MT.AS"], ["Vinci", "DG.PA"], ["Bouygues", "EN.PA"], ["Saint-Gobain", "SGO.PA"]]
+indegal = [a,b,c,d,e,f,g,h]
